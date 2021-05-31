a152624afd8d0170927ae3da96b1599414a8541c
elakia <vmelakia@gmail.com>
2021-05-31 20:21:48 +0530

    hello added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def start ():
-    return render_template('index1.html')
+    return 'HELLO'
 
 
 if __name__ =="__main__":
