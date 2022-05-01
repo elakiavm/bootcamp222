b4076338bfdb57c9f89d2e7cfc71c6083b4c223f
elakia <vmelakia@gmail.com>
2022-05-01 10:27:58 +0530

    updated

--- flask-with-jinja2/app1.py
+++ flask-with-jinja2/app1.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask ,render_template 
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
-def start ():
-    return render_template('index1.html',name="elakia")
+# @app.route('/')
+# def start():
 
+#     return render_template('index1.html',result = None )
+
+@app.route("/",methods=["GET","POST"])
+
+def planet ():
+    
+    result = {
+        "1" :"hello"    
+    }
+    return render_template("index1.html",output=result)
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port = 3021)
